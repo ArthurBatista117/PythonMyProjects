64ff883637300781c86245ea87821d7f9c9e5bdb
ArthurBatista117 <167575173+ArthurBatista117@users.noreply.github.com>
2025-01-28 19:42:48 -0300

    .

--- bank/Bank.py
+++ bank/Bank.py
@@ -1,3 +1,5 @@
+import csv
+
 class Bank:
     def __init__(self, n_account, balance=0):
         self.n_account = n_account
@@ -21,6 +23,45 @@ class Bank:
     def get(cls):
         num_account = input('Number of account: ')
         return cls(num_account)
+
+    def check(self):
+        with open('accounts.csv', 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+
+                if self.n_account in row:
+                    r = input('This account has exist, you want make a deposit here? ')
+
+                    while r.upper() not in 'YN':
+                        r = input('This account has exist, you want make a deposit here? ')
+
+                    if r.upper() in 'Y':
+                        self.deposit(int(input('Valor: ')))
+
+                    elif r.upper() in 'N':
+                        r = input('You want make a windrow here?[Y/N] ')
+
+                        while r.upper() not in 'YS':
+                            r = input('You want make a windrow here?[Y/N] ')
+
+                        if r.upper() in 'Y':
+                            self.deposit(int(input('Valor: ')))
+                        else:
+                            print('Create a new account for progress')
+
+    def append(self):
+        with open('accounts.csv', 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if self.n_account in row:
+                    raise ValueError('This number of account has exist')
+
+        with open('accounts.csv', '+a') as file:
+            header = ['Number Account', 'Balance']
+            writer = csv.DictWriter(file, fieldnames=header)
+            writer.writeheader()
+            writer.writerow({'Number Account': self.n_account, 'Balance': self._balance})
+
 
     def deposit(self, n):
         self._balance += n
--- bank/main.py
+++ bank/main.py
@@ -1,31 +1,10 @@
 from Bank import Bank
-import csv
 
 def main():
     account = Bank.get()
-    with open('accounts.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-
-            if account.n_account in row:
-                r = input('This account has exist, you want make a deposit here? ')
-
-                while r.upper() not in 'YN':
-                    r = input('This account has exist, you want make a deposit here? ')
-
-                if r.upper() in 'Y':
-                    account.deposit(int(input('Valor: ')))
-
-                elif r.upper() in 'N':
-                    r = input('You want make a windrow here?[Y/N] ')
-
-                    while r.upper() not in 'YS':
-                        r = input('You want make a windrow here?[Y/N] ')
-
-                    if r.upper() in 'Y':
-                        account.deposit(int(input('Valor: ')))
-                    else:
-                        print('Create a new account for progress')
-
+    account.check()
+    account.deposit(1000)
+    account.windrow(100)
+    account.append()
 if __name__ == '__main__':
     main()
