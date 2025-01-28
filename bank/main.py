2fce0cba025270013b9e31e4140864211c6c6cc5
ArthurBatista117 <167575173+ArthurBatista117@users.noreply.github.com>
2025-01-28 19:04:55 -0300

    .

--- bank/main.py
+++ bank/main.py
@@ -1,11 +1,31 @@
 from Bank import Bank
+import csv
 
 def main():
     account = Bank.get()
-    account.deposit(int(input('Deposit: ')))
-    account.windrow(50)
-    print(account.__str__())
+    with open('accounts.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
 
+            if account.n_account in row:
+                r = input('This account has exist, you want make a deposit here? ')
+
+                while r.upper() not in 'YN':
+                    r = input('This account has exist, you want make a deposit here? ')
+
+                if r.upper() in 'Y':
+                    account.deposit(int(input('Valor: ')))
+
+                elif r.upper() in 'N':
+                    r = input('You want make a windrow here?[Y/N] ')
+
+                    while r.upper() not in 'YS':
+                        r = input('You want make a windrow here?[Y/N] ')
+
+                    if r.upper() in 'Y':
+                        account.deposit(int(input('Valor: ')))
+                    else:
+                        print('Create a new account for progress')
 
 if __name__ == '__main__':
     main()
